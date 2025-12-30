9690861b4986278763b195f1bf945d5c435de1bc
Ntdieu02 <nt.dieu@hotmal.com>
2025-12-30 15:48:35 +0200

    Mise à jour du gitignore et nettoyage des fichiers sensibles

--- wp-content/plugins/wp-fastest-cache-master-860e6e48b0feb92e48b716de439545e99e471968/includes/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Silence is golden.
- *
- * @package    fastest-cache
- */
-
-// Silence.
--- wp-content/themes/aravalli-personal/inc/cpt/cpt-main.php
+++
@@ -1,4 +0,0 @@
-<?php
-	require( get_template_directory() . '/inc/cpt/aravalli-cpt-room.php');
-	require( get_template_directory() . '/inc/cpt/aravalli-cpt-packages.php');
-?>
